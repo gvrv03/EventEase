65893dbe9b6c72ab222734d023a714166a0cb8e1
gvrv03 <itsgaurav3112003@gmail.com>
2025-01-07 14:53:45 +0530

    Add Requiured folders

---
+++ src/app/(root)/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePAge = () => {
+  return (
+    <>
+      <div>Homepage</div>
+      <div>Homepage</div>
+      <div>Homepage</div>
+      <div>Homepage</div>
+    </>
+  );
+};
+
+export default HomePAge;
---
+++ src/app/Event/page.js
@@ -0,0 +1,13 @@
+import EventCard from '@/coponents/Events/EventCard'
+import React from 'react'
+
+const EventPage = () => {
+  return (
+    <div>
+      <EventCard/>
+    
+    </div>
+  )
+}
+
+export default EventPage
